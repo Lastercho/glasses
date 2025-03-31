abc70cdf9c701a8bf9d424b261bd7c6b7a04c2ce
Stoyan <s.imandiev@is-bg.net>
2025-03-31 17:21:48 +0300

    GitPages setings and env changes

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-undef */
 import pkg from 'pg';
 const { Pool } = pkg;
-import dotenv from 'dotenv';
+import 'dotenv/config'
 
 
-dotenv.config();
+
 
 const pool = new Pool({
     user: process.env.DB_USER,
